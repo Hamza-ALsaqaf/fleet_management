877899eae41a32f4f489511bac4ee5e44bc433c7
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-23 04:00:36 +0530

    flat: Create DocTypes for managing Vehicle bookings

---
+++ fleet_management/fleet_management/doctype/ride_item/ride_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Hamza Alsaqaf and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class RideItem(Document):
+	pass
