7e8e56ece4035f8e4a1eee07fbbd4c8cb414c8b7
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-23 06:26:18 +0530

    feat: patch that alter the existing vehicle title

---
+++ fleet_management/fleet_management/doctype/vehicle/patches/set_title.py
@@ -0,0 +1,9 @@
+#any patche file must contain function called execute()
+import frappe
+def execute():
+    vehicles=frappe.db.get_all("Vehicle",pluck="name")
+    for v in vehicles:
+        vehicle=frappe.get_doc("Vehicle",v)
+        vehicle.set_title()
+        vehicle.save()
+    frappe.db.commit()
